e1f26c800497af77ed9a68eede95b8e8d7455153
scott-fryxell <scott-fryxell@users.noreply.github.com>
2021-11-09 18:03:59 +0000

    deploy: afcd9acc3f244e5e45ff4f5f9e3960b35b49f8e3

---
+++ _nuxt/static/1636481038/blog/tooling-saves-your-bacon/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636481038",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftooling-saves-your-bacon",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c1c27a90"}}}}(null));
