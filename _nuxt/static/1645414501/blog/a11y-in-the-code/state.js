4d681dce1ea5e7c34ec6ac867b959b3535a86d30
scott-fryxell <scott-fryxell@users.noreply.github.com>
2022-02-21 21:54:31 +0000

    deploy: 4d6ba653d1fffdb18dda0d09996d9dd06fed3cd4

--- _nuxt/static/1645414501/blog/a11y-in-the-code/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645414501",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fa11y-in-the-code",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cf3e537f"}}}}(null));
