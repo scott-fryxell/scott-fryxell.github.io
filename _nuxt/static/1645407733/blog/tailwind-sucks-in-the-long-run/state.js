f789689197adec24810c3898191aff23e513e1f8
scott-fryxell <scott-fryxell@users.noreply.github.com>
2022-02-21 01:42:15 +0000

    deploy: 5c2d0152f293210f0e0232da5353513e85e15210

---
+++ _nuxt/static/1645407733/blog/tailwind-sucks-in-the-long-run/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645407733",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftailwind-sucks-in-the-long-run",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dd1d7ce2"}}}}(null));
