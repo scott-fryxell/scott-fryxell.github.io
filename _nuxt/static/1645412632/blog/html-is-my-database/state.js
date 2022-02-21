e0d98c4d348e9af3e0f7d66e7510acec73ace764
scott-fryxell <scott-fryxell@users.noreply.github.com>
2022-02-21 03:35:02 +0000

    deploy: 233445c692b3d80aa113eb8d5be290fa682df95e

--- _nuxt/static/1645412632/blog/html-is-my-database/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645412632",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhtml-is-my-database",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c93709a9"}}}}(null));
--- _nuxt/static/1645412632/blog/tailwind-sucks-in-the-long-run/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645412632",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftailwind-sucks-in-the-long-run",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c93709a9"}}}}(null));
