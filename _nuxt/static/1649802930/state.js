9ce4673423c84856c9086cfa75ea5c888564fa03
scott-fryxell <scott-fryxell@users.noreply.github.com>
2022-04-12 22:35:31 +0000

    deploy: e9acd802ae225ce5751e5c4a4ff925ac47062a4e

---
+++ _nuxt/static/1649802930/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649802930",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"987ba3db"}}}}(null,"\u002F"));
