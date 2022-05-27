a37cc1dfb7b8c0274d68e48ae22d48e87e894d55
Scott Fryxell <sfryxell@me.com>
2022-05-26 17:29:23 -0700

    Add a little color gradient to title text

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,26 +7,32 @@
         <a hidden href="https://cash.app/$ScottFryxell">$</a>
       </nav>
     </header>
-    <Nuxt/>
-    <footer/>
+    <Nuxt />
+    <footer />
   </main>
 </template>
 <style lang="stylus">
-  [outline],
-  [outline] *
-    outline: 0.15vmin dashed grey
-  main
-    margin: auto
-    flex-direction: column
-    & > header
-      display: flex
-      & > h1
-        padding: 1rem
-      & > nav
-        align-self: center
-        & > a
-          margin-right: 1rem
-    & > footer
-      padding: 1em
+[outline],
+[outline] *
+  outline: 0.15vmin dashed grey
+main
+  margin: auto
+  flex-direction: column
+  h1, h2, h3
+    background: -webkit-linear-gradient(315deg,red 25%,blue)
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: hsl(0deg 0% 0% / 0%);
 
+  & > header
+    display: flex
+    & > h1
+      padding: 1rem
+
+    & > nav
+      align-self: center
+      & > a
+        margin-right: 1rem
+  & > footer
+    padding: 1em
 </style>
