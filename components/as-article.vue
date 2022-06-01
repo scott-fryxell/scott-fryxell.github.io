6df89ed679777c1f1a18e2655d9a373515edbb29
Scott Fryxell <sfryxell@me.com>
2022-06-01 15:43:11 -0700

    Lazy load graphics

--- components/as-article.vue
+++ components/as-article.vue
@@ -1,7 +1,12 @@
 <template>
   <article class="post">
     <header>
-      <img v-if="article.img" :src="article.img" :alt="article.alt" />
+      <img
+        v-if="article.img"
+        :src="article.img"
+        :alt="article.alt"
+        loading="lazy"
+      />
       <h2>
         <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">{{
           format_title(article.slug)
@@ -14,55 +19,55 @@
   </article>
 </template>
 <script>
-  export default {
-    props: {
-      article: {
-        type: Object,
-        required: true,
-      },
+export default {
+  props: {
+    article: {
+      type: Object,
+      required: true,
     },
-    methods: {
-      format_date(date) {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' }
-        return new Date(date).toLocaleDateString('en', options)
-      },
-      format_title(slug) {
-        return slug.replace(/-/g, ' ')
-      },
+  },
+  methods: {
+    format_date(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
     },
-  }
+    format_title(slug) {
+      return slug.replace(/-/g, ' ')
+    },
+  },
+}
 </script>
 <style lang="stylus">
-  article.post
-    display: flex
-    flex-direction: column
-    justify-content: center
-    align-items: center;
-    & > header
-      & > img
-        width: 70vw
-        max-height: 100vh
-      & > h2
-        width: 30vw
-        padding: 0 1rem
-        text-transform: capitalize
-        & > a
-          // color: inherit
-    & > details
-      display: inline-block
+article.post
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center;
+  & > header
+    & > img
+      width: 70vw
+      max-height: 100vh
+    & > h2
+      width: 30vw
+      padding: 0 1rem
+      text-transform: capitalize
+      & > a
+        // color: inherit
+  & > details
+    display: inline-block
+    margin-left: 1rem
+    summary
+      font-size: small
+      opacity:0.4
+    & > nav > a
+      text-decoration: none
+      line-height: 2
       margin-left: 1rem
-      summary
-        font-size: small
-        opacity:0.4
-      & > nav > a
-        text-decoration: none
-        line-height: 2
-        margin-left: 1rem
-        display: block
-    & > footer
-      padding: 1em
-    .nuxt-content,
-    .nuxt-content-editor
-      // margin: auto
-      max-width: 33rem
+      display: block
+  & > footer
+    padding: 1em
+  .nuxt-content,
+  .nuxt-content-editor
+    // margin: auto
+    max-width: 33rem
 </style>
