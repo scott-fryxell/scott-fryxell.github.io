9feca35a834212b89ab7599fdb9f5b916485b273
scott-fryxell <scott-fryxell@users.noreply.github.com>
2022-07-16 20:53:33 +0000

    deploy: 4fcae1341542b7515f18ae7765afd31e4da2d0ad

---
+++ _nuxt/static/1658004812/blog/pin-prick/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658004812",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fpin-prick",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"762bcb18"}}}}(null));
