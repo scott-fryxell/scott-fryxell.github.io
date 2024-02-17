8b9283bbc94e4504a9dbdf8c8b31da7b88fc2810
scott-fryxell <scott-fryxell@users.noreply.github.com>
2024-02-17 21:03:04 +0000

    deploy: 4dbfae4773503c4f3f596710d9f70b0912a24a82

---
+++ _nuxt/static/1708203783/blog/html-is-my-database/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1708203783",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhtml-is-my-database",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a9df0ef"}}}}(null));
