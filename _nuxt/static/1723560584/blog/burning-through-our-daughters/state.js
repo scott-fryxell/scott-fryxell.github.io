57d391a0fe1ef8a967071f9351d529bccd3442c5
scott-fryxell <scott-fryxell@users.noreply.github.com>
2024-08-13 14:49:45 +0000

    deploy: 4b7f038b34aa7c51985e2181c8ddaa593250791c

---
+++ _nuxt/static/1723560584/blog/burning-through-our-daughters/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1723560584",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fburning-through-our-daughters",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9066bdf1"}}}}(null));
